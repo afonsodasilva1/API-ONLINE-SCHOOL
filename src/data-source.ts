4901e1bec80d757de4403f7011dd6a12c1c4935f
Afonso Da Silva Vicente <afonsodasilva0308@gmail.com>
2023-10-23 22:52:54 +0100

    feat: 1st Config
    Primeira configuração do projecto, instalei as dependências necessárias e a conexão com o db

---
+++ src/data-source.ts
@@ -0,0 +1,12 @@
+import { DataSource } from 'typeorm';
+
+//const port = process.env.DB_PORT as number || undefined
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: process.env.DB_HOST,
+    port: 5432,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  })
